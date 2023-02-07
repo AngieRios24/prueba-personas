3ed98645a7ac337ebd8a961bc0fb92a19366a7a1
DAT-PT09 <angie.rios@datasoft.com.co>
2023-02-07 14:55:47 -0500

    ejemplo

---
+++ personas/personas.component.ts
@@ -0,0 +1,50 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+// import { Chart } from 'chart.js';
+// import { PeticionesService } from 'src/app/peticiones.service';
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  styleUrls: ['./personas.component.css']
+})
+export class PersonasComponent implements OnInit {
+  canvas: any;
+  barChart:any;
+  //@ViewChild('canvas1') canvas1: ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+ /* ngAfterViewInit() {
+    let ctx = this.canvas.nativeElement.getContext('2d');
+    this.barChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+    
+    });
+  }*/
+
+}
